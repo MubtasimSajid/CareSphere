e27dced2cdd894f83cd09a927f96ead1508aa220
ishmam259 <tahmid12955@gmail.com>
2025-02-26 18:51:19 +0600

    UI

--- CareSphere/main.cpp
+++ CareSphere/main.cpp
@@ -1,11 +1,15 @@
 #include "caresphere.h"
+#include<iostream>
 
 #include <QApplication>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     CareSphere w;
     w.show();
+    cout<<"Hello World"<<endl;
+
     return a.exec();
 }
