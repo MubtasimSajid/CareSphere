26168a21a0eb67a4303b09fe61d5bcdc505af796
Mubtasim Sajid Ahmed Arian <mubtasimsajidahmedarian.11@gmail.com>
2025-03-14 10:24:45 +0600

    Minor fixes

--- CareSphere/main.cpp
+++ CareSphere/main.cpp
@@ -17,24 +17,8 @@ int main(int argc, char *argv[]) {
     Login loginWindow;
     loginWindow.show();
 
-    // Testing the User Functions
     vector<User> users;
     MySQLinit();
-    // User test("Farhan-01", "Farhan", "ayon55928@gmail.com", "01735429709", "B+", "Male", "01-12-2001");
-    // test.details();
-    // saveUser(test);
-    // users = loadUsers();
-    // for(auto x: users){
-    //     x.details();
-    // }
-    // users = loadUsers();
-    // User current_user = getUser(users, "Farhan-01");
-    // qInfo()<<"Printing the details of the User Found using the function";
-    // qInfo()<<"Name of the User: "<<current_user.getName();
-    // qInfo() << "Email of the User: " << QString::fromStdString(current_user.getEmail());
-    // qInfo() << "Phone Number of the User: " << QString::fromStdString(current_user.getPhoneNo());
-    // qInfo() << "Gender of the User: " <<  QString::fromStdString(current_user.getGender());
-    // qInfo() << "Date of Birth of the User: " << QString::fromStdString(current_user.getDOB());
 
     return a.exec();
 }
