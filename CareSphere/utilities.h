fe1a99f2f6f7968363d92120928b26bc36b98963
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-03-14 23:53:11 +0600

    Completed the Notes Section of The Patient

--- CareSphere/utilities.h
+++ CareSphere/utilities.h
@@ -2,6 +2,7 @@
 #define UTILITIES_H
 
 #include "user.h"
+#include "MySQL_utilities.h"
 #include <string>
 #include <QCoreApplication>
 #include <QSqlDatabase>
@@ -24,6 +25,38 @@ inline string encryptPassword(const string& password) {
 // Function to decrypt the password (XOR is reversible)
 inline string decryptPassword(const string& encryptedPassword) {
     return encryptPassword(encryptedPassword);  // XOR again to get the original password
+}
+
+inline void save_User_Notes(const string &user_name, const string &notes){
+    QSqlQuery query;
+    QString insert_user = "INSERT INTO NOTES (user_id, notes) VALUES ('"
+                          + QString::fromStdString(user_name) + "', '"
+                          + QString::fromStdString(notes) + "' )";
+
+    MySQL_Insert(insert_user);
+}
+
+inline std::string get_User_Notes(const std::string &username) {
+    QSqlQuery query;
+
+    // Query to get all notes for the given user_id
+    query.prepare("SELECT notes FROM NOTES WHERE user_id = :username");
+    query.bindValue(":username", QString::fromStdString(username));
+
+    if (!query.exec()) {
+        qDebug() << "Query failed: " << query.lastError().text();
+        return "";  // Return an empty string on failure
+    }
+
+    std::string all_notes;
+    while (query.next()) {
+        if (!all_notes.empty()) {
+            all_notes += "\n";  // Add newline separator between notes
+        }
+        all_notes += query.value(0).toString().toStdString();
+    }
+
+    return all_notes.empty() ? "No notes found for this user." : all_notes;
 }
 
 #endif
