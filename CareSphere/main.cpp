201921f2f5bd0d35c5a9858bbd270ca46c776376
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-03-14 09:36:09 +0600

    1. Fixed the DB Functions Save_User and Load_Users. 2. Fixed the GetUser Function

--- CareSphere/main.cpp
+++ CareSphere/main.cpp
@@ -34,5 +34,21 @@ int main(int argc, char *argv[]) {
     // qInfo() << "Religion of the User: " << religionToString( current_user.getReligion());
     // qInfo() << "Date of Birth of the User: " << QString::fromStdString(current_user.getDOB());
 
+    // User test("Farhan-01", "Farhan", "ayon55928@gmail.com", "01735429709", "B+", "Male", "01-12-2001");
+    // test.details();
+    // saveUser(test);
+    // users = loadUsers();
+    // for(auto x: users){
+    //     x.details();
+    // }
+    // users = loadUsers();
+    // User current_user = getUser(users, "Farhan-01");
+    // qInfo()<<"Printing the details of the User Found using the function";
+    // qInfo()<<"Name of the User: "<<current_user.getName();
+    // qInfo() << "Email of the User: " << QString::fromStdString(current_user.getEmail());
+    // qInfo() << "Phone Number of the User: " << QString::fromStdString(current_user.getPhoneNo());
+    // qInfo() << "Gender of the User: " <<  QString::fromStdString(current_user.getGender());
+    // qInfo() << "Date of Birth of the User: " << QString::fromStdString(current_user.getDOB());
+
     return a.exec();
 }
