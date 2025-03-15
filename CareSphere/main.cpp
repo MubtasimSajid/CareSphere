d5e85b70b944edf7f536f2a82652647471e1ba38
Farhan41229 <farhankhan@iut-dhaka.edu>
2025-03-15 09:49:06 +0600

    Made the Prescriptions to be in the same format as the app

--- CareSphere/main.cpp
+++ CareSphere/main.cpp
@@ -13,26 +13,11 @@ int main(int argc, char *argv[]) {
 
     MySQLinit();
 
-    // // Testing the Prescription Class
-    // // Create an object using parameterized constructor
-    // Prescription p1("F-00", "PARACETAMOL", "500mg", "2 Times after Eating");
-    // Prescription p2("F-00", "ACE PLUS", "200mg", "3 Times after Eating");
-
-    // SavePrescription(p1);
-    // SavePrescription(p2);
-
-    // // Testing the Get Prescription function to get PRescription from the DB.
-    // string test = GetUserPrescriptions("F-00");
-    // cout<<test<<endl;
-
-    string notes = "1. Avoid spending too much time on mobile devices\n"
-                   "2. Drink 3L water every day\n"
-                   "3. Eat Fruits to replenish the Vitamin deficiencies";
-
-    // qInfo()<<notes;
-    // save_User_Notes("F-00", notes);
-    string test = get_User_Notes("F-00");
-    cout<<test<<endl;
+    Prescription test("F-00", "WAZED", "ACE-PLUS, PERACETAMOL, NAPA");
+    test.displayDetails();
+    // SavePrescription(test);
+    string check = GetUserPrescriptions("F-00");
+    cout<<check<<endl;
 
 
     Login loginWindow;
