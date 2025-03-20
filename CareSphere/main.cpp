fe03164fa3f983429022655657395b11c86dae3f
Mubtasim Sajid Ahmed Arian <mubtasimsajidahmedarian.11@gmail.com>
2025-03-21 02:50:41 +0600

    Cleaned main.cpp

--- CareSphere/main.cpp
+++ CareSphere/main.cpp
@@ -17,47 +17,6 @@ int main(int argc, char *argv[]) {
     MySQLinit();
     QTextStream out(stdout);
 
-    // // Testing Prescriptions
-    // Prescription test("F-00", "WAZED", "ACE-PLUS, PERACETAMOL, NAPA");
-    // // // test.displayDetails();
-    // // SavePrescription(test);
-    // DeletePrescription("F-00", "WAZED", "ACE-PLUS, PERACETAMOL, NAPA");
-    // // UpdatePrescription("F-00", "FTK", "No Medicines", "WAZED", "ACE-PLUS, PERACETAMOL, NAPA");
-    // vector<string> check = GetUserPrescriptions("F-00");
-    // for(auto x: check){
-    //     qInfo()<<x;
-    // }
-
-
-    // Testing Notes
-    // string notes = "Eat Apple";
-    // save_User_Notes("new3", "banana");
-    // vector<string> Notes = get_User_Notes("new3");
-    // for (auto x: Notes){
-    //     cout<<x<<endl;
-    // }
-    // Delete_User_Notes("new3", "banana");
-    // string check2 = get_User_Notes("new3");
-    // cout<<check2<<endl<<endl;
-
-    // // Testing Reminders
-    // // Creating a reminder object
-    // Reminder r1("F-00", "Doctor Appointment", "2025-12-01", "12:30:00", "Visit Dr. Smith for a checkup");
-    // Save_User_Reminder(r1);
-    // Update_Reminder("F-00", "Test", "2025-30-01", "13:00", "Test Is running","Doctor Appointment","2025-12-01","12:30:00", "Visit Dr. Smith for a checkup"   );
-    // Delete_Reminder("F-00", "Test", "2025-30-01", "13:00", "Test Is running");
-
-    // // Testing Appointments
-    // // Creating an appointment object
-    // // Appointment a1("F-00", "Test", "Mirpur", "01-12-2025", "9:00 PM");
-    // // Save_User_Appointment(a1);
-    // Update_User_Appointment("F-00", "Test-Doc", "testlocation", "01-12-2003", "12:00 PM", "Wazed", "Mohmaddpur", "01-12-2025", "9:00 PM");
-    // vector<string> check = Get_User_Appointments("F-00");
-    // for(auto x: check){
-    //     qInfo()<<x;
-    // }
-    Delete_User_Appointment("F-00", "Test", "Mirpur", "01-12-2025", "9:00 PM");
-
     Login loginWindow;
     loginWindow.show();
 
